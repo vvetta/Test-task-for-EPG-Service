8399938dad0d06a69ca5bf94aa8d0f9056a78df3
Nikita <drpsih2004@gmail.com>
2024-10-28 21:03:43 +0300

    Ready-made client model with the ability to specify location (latitude, longitude)

---
+++ src/api/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import String, Float
+from sqlalchemy.orm import mapped_column
+
+from src.api.database import BaseModel
+
+
+class Client(BaseModel):
+
+    fio = mapped_column(String(length=64), nullable=False)
+    email = mapped_column(String(length=64), nullable=False, unique=True)
+    gender = mapped_column(String(length=10), nullable=False, default="не выбрано")
+    photo = mapped_column(String(length=256), nullable=True)
+
+    """
+        Фото пользователя будет хранится в отдельной папке в приложении.
+        В самой базе данных будет находится только путь до фото пользователя.
+    """
+
+    password = mapped_column(String(length=256), nullable=False)
+    longitude = mapped_column(Float, nullable=True)
+    latitude = mapped_column(Float, nullable=True)
+
